8a865e85f16330a561d9fbd27b9c84630141c526
dmitriileonov93 <dimaleonov93@yandex.ru>
2022-05-05 11:56:27 +0300

    fix

--- cats/serializers.py
+++ cats/serializers.py
@@ -1,10 +1,62 @@
 from rest_framework import serializers
+import datetime as dt
+import webcolors
 
-from .models import Cat
+from .models import Achievement, AchievementCat, Cat, Owner, CHOICES
+
+
+class AchievementSerializer(serializers.ModelSerializer):
+    achievement_name = serializers.CharField(source='name')
+
+    class Meta:
+        model = Achievement
+        fields = ('id', 'achievement_name')
 
 
 class CatSerializer(serializers.ModelSerializer):
+    achievements = AchievementSerializer(many=True, required=False)
+    age = serializers.SerializerMethodField()
+    color = serializers.ChoiceField(choices=CHOICES)
 
     class Meta:
         model = Cat
-        fields = ('id', 'name', 'color', 'birth_year')
+        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements',
+                  'age')
+
+    def get_age(self, obj):
+        return dt.datetime.now().year - obj.birth_year
+
+    def create(self, validated_data):
+        # Если в исходном запросе не было поля achievements
+        if 'achievements' not in self.initial_data:
+            # То создаём запись о котике без его достижений
+            cat = Cat.objects.create(**validated_data)
+            return cat
+        else:
+            achievements = validated_data.pop('achievements')
+            # Иначе сначала добавляем котика в БД
+            cat = Cat.objects.create(**validated_data)
+            # А потом добавляем его достижения в БД
+            for achievement in achievements:
+                current_achievement, status = Achievement.objects.get_or_create(
+                    **achievement)
+                # И связываем каждое достижение с этим котиком
+                AchievementCat.objects.create(
+                    achievement=current_achievement, cat=cat)
+            return cat
+
+
+class CatListSerializer(serializers.ModelSerializer):
+    color = serializers.ChoiceField(choices=CHOICES)
+
+    class Meta:
+        model = Cat
+        fields = ('id', 'name', 'color')
+
+
+class OwnerSerializer(serializers.ModelSerializer):
+    cats = serializers.StringRelatedField(many=True, read_only=True)
+
+    class Meta:
+        model = Owner
+        fields = ('first_name', 'last_name', 'cats')
